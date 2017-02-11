ed3b5ce534d4de02930578885d36e9678fae8dde
Artem Arshaniy <artem.arshaniy108@gmail.com>
2017-02-11 22:20:11 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+
+function pageDown() {
+   scrollBy(0,740)
+  }
+
+
+
+// var inform1 = document.getElementById('inform1')
+// var inform2 = document.getElementById('inform2')
+// var inform3 = document.getElementById('inform3')
+// var inform4 = document.getElementById('inform4')
+// var inform5 = document.getElementById('inform5')
+//
+// function alert() {
+//   document.getElementById('inform1').style.visibility = 'inherit';
+
+// var arr = [inform1, inform2, inform3, inform4, inform5]
+// for button down
+// $(document).ready(function(){
+// 2
+//         $(window).scroll(function(){
+// 3
+//             var bo = $("body").scrollTop();
+// 4
+//         if ( bo > 200 ) $("#hid").animate({'opacity':'1'},500);
+// 5
+//         })
+// 6
+//     })
