a067e99caf76cdfdca26a425cadd5835de2369a6
Artem Arshaniy <artem.arshaniy108@gmail.com>
2017-03-07 14:30:19 +0200

    add btn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-$('.carousel').carousel()
 
+  var aboutBtn = document.getElementById('about');
+  var uslugiBtn = document.getElementById('uslugi');
+  var priceBtn = document.getElementById('price');
+ var contactBtn = document.getElementById('contact');
+    aboutBtn.onclick = function () {
+      scrollBy(0, 700);
+    };
 
-function pageDown() {
-   scrollBy(0,800)
-  }
+    uslugiBtn.onclick = function () {
+      scrollBy(0, 1000);
+    };
+
+    priceBtn.onclick = function () {
+      scrollBy(0, 1300);
+    };
+
+    contactBtn.onclick = function () {
+      scrollBy(0, 1600);
+    };
